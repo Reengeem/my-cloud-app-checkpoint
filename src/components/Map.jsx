d25ca290438a5fef5909b270271447e0f5c99087
Adams Unity <128307592+AdamsUnity@users.noreply.github.com>
2024-01-08 12:32:11 +0100

    first commit

---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import "./map.css";
+import LocationPin from "./LocationPin";
+
+const Map = ({ location, zoomLevel }) => {
+  return (
+    <div className="map">
+      <h2 className="map-h2">Visit Us At</h2>
+      <div className="google-map">
+        {/* Calling google map component from google-map-react */}
+        <GoogleMapReact
+          //Calling the google api key
+          bootstrapURLKeys={{ key: "AIzaSyAX5P9Kpnf4IT_2GdTBGeZBfB6IFmdEoYc" }}
+          defaultCenter={location}
+          defaultZoom={zoomLevel}
+        >
+          {/* Calling the Location pin component */}
+          <LocationPin
+            lat={location.lat}
+            lng={location.lng}
+            text={location.address}
+          />
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
